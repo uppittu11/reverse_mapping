e3371bf642d99c07dcb8ea1059d7ee4e9eda02ae
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-10-03 17:20:24 -0500

    Added specific prototype subclass for ceramides

--- reverse_mapping/simplified/prototype.py
+++ reverse_mapping/simplified/prototype.py
@@ -91,3 +91,37 @@ class Prototype(object):
     def compound(self, compound):
         assert isinstance(compound, mb.Compound)
         self._compound = compound
+
+class CeramidePrototype(Prototype):
+    """
+    Class to store information about each ceramide molecule type.
+
+    Stores information about the indices for the heads/tails to be used
+    to classify lipid moleules. Also stores the atomistic
+    prototypes corresponding to the compound. Allows 
+
+    Args:
+    -----------
+    name : string
+        The name of the molecule
+    cg_head : int
+        The index for the head bead of the CG lipid
+    aa_head : int
+        The index for the head bead of the atomistic lipid
+    tail : int
+        The index for the terminal tail bead of the CG lipid
+    atomlist : list
+        A list of atoms in the CG molecule. Can be used to identify
+        CG atoms
+    compound : mb.Compound
+        The location of the atomistic prototype structure
+    
+    """
+    @property
+    def tail(self):
+        return self._tail
+    
+    @tail.setter
+    def tail(self, index):
+        index = list(index)
+        self._head = index
